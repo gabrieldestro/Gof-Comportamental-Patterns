11a07d325443c908633a6580a7e18fbdd9e1a864
gdestro <gabrielaugustodestro@gmail.com>
2025-05-07 18:42:24 -0300

    Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿using TemplateMethod;
+
+Console.WriteLine("Playing Mp4 video.");
+VideoPlayer vpMp4 = new VideoMp4();
+vpMp4.ExecuteVideo();
+
+Console.WriteLine("\nPlaying Mkv video.");
+VideoPlayer vpMkv = new VideoMkv();
+vpMkv.ExecuteVideo();
---
+++ TemplateMethod/VideoPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public abstract class VideoPlayer
+    {
+        public abstract void DecodeFormat();
+
+        public void ExecuteVideo()
+        {
+            LoadVideo();
+            DecodeFormat();
+            InitReprodution();
+        }
+
+        public void LoadVideo()
+        {
+            Console.WriteLine("Loading video.");
+        }
+
+        public void InitReprodution()
+        {
+            Console.WriteLine("Initializing video reproduction.");
+        }
+    }
+}
