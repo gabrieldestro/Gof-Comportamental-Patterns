6da1845c2bd7053f9ac6a501e3911940cfcb12be
gdestro <gabrielaugustodestro@gmail.com>
2025-05-07 18:29:45 -0300

    strategy

---
+++ Strategy/CompressionContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class CompressionContext
+    {
+        private ICompression _compression;
+
+        public CompressionContext(ICompression compression)
+        {
+            _compression = compression;
+        }
+
+        public void DefineStrategy(ICompression compression)
+        {
+            _compression = compression;
+        }
+
+        public void CompressFile(string fileName) => _compression.CompressFile(fileName);
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,10 @@
+﻿using Strategy;
+
+CompressionContext ctx = new CompressionContext(new CompressionRar());
+ctx.CompressFile("file.txt");
+
+ctx.DefineStrategy(new CompressionZip());
+ctx.CompressFile("file.txt");
+
+ctx.DefineStrategy(new CompressionGzip());
+ctx.CompressFile("file.txt");
